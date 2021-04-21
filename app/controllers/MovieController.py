3569779e40ee634937cac9ac01b3de1f47ad05b0
serabutantekno <madyaneka@protonmail.com>
2021-04-22 06:22:53 +0700

    bugfix: retrieving all movies

--- app/controllers/MovieController.py
+++ app/controllers/MovieController.py
@@ -1,3 +1,5 @@
+from flask import request
+from app import db
 from app.models import MovieModel
 
 
@@ -6,8 +8,18 @@ class MovieController:
 
     def get_all_movies(self):
         all_movies_obj = MovieModel.Movie.query.all()
-        list_all_movies_obj = list(all_movies_obj)
+        list_all_movies_obj = [movie.data_to_json() for movie in all_movies_obj]
         return {
             "message": "retrieving all movies success",
             "data": list_all_movies_obj
         }
+
+
+    def create(self):
+        form = request.form
+        if form:
+            data = form.to_dict()
+            post = MovieModel.Movie(**data)
+            db.session.add(post)
+            db.session.commit()
+            return "oke"
