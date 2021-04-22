4a1c0a34180ba407159b901c54d408e2291fc3f4
serabutantekno <madyaneka@protonmail.com>
2021-04-22 07:05:03 +0700

    create base response

--- app/controllers/MovieController.py
+++ app/controllers/MovieController.py
@@ -1,10 +1,13 @@
 from flask import request
 from app import db
+from app.controllers.BaseResponseController import BaseResponse
 from app.models import MovieModel
 
 
 class MovieController:
     """Movie Controller"""
+
+    RESPONSE = BaseResponse()
 
     def get_all_movies(self):
         all_movies_obj = MovieModel.Movie.query.all()
@@ -18,12 +21,21 @@ class MovieController:
     def create(self):
         form = request.form
         if form:
-            data = form.to_dict()
-            post = MovieModel.Movie(**data)
-            db.session.add(post)
-            db.session.commit()
-            return MovieModel.Movie.data_to_json(post)
+            try:
+                data = form.to_dict()
+                post = MovieModel.Movie(**data)
+                db.session.add(post)
+                db.session.commit()
+                result = MovieModel.Movie.data_to_json(post)
+                return self.RESPONSE.base_response(message="a new movie added", data=result, status_code=201)
+            except Exception as error:
+                print(error)
+                return self.RESPONSE.error()
 
 
     def get(self, id):
-        return MovieModel.Movie.query.get(id).data_to_json()
+        try:
+            return MovieModel.Movie.query.get(id).data_to_json()
+        except Exception as error:
+            print(error)
+            return self.RESPONSE.data_not_found()
